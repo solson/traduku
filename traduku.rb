7b3edee63fb2dd2fec19b0ce30801ac30b766684
Scott Olson <scott@scott-olson.org>
2012-02-27 18:42:10 -0600

    Use Configru gem for configuration.

--- traduku.rb
+++ traduku.rb
@@ -5,9 +5,7 @@ require 'cgi'
 require 'nokogiri'
 require 'bing_translator'
 require 'cinch'
-
-# Max length for chained translation (lang1 -> lang2 -> ... -> langN)
-MAX_CHAIN_LENGTH = 5
+require 'configru'
 
 TRADUKU_URL = URI.parse('http://traduku.net/cgi-bin/traduku')
 
@@ -16,8 +14,88 @@ TRADUKU_URL = URI.parse('http://traduku.net/cgi-bin/traduku')
 LERNU_LANGS = %w[ar bg ca cs da de el en es eo fa fi fr ga he hi hr hsb hu it
                  ja ko lt nl no pl pt ro ru sk sl sr sv sw th tr uk vi zh]
 
-bt = BingTranslator.new(File.read("bing_api_key").chomp)
-BING_LANGS = bt.supported_language_codes
+Configru.load do
+  just 'config.yml'
+
+  defaults do
+    port 6667
+    max_chain_length 5
+  end
+end
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server   = Configru.server
+    c.port     = Configru.port
+    c.nick     = Configru.nick
+    c.channels = Configru.channels
+
+    @max_chain_length = Configru.max_chain_length
+    @bing = BingTranslator.new(Configru.bing_api_key)
+    BING_LANGS = @bing.supported_language_codes
+  end
+
+  on :message, /^#{Regexp.escape(nick)}\S*[:,]\s*(.+)$/ do |m, word|
+    command, args = word.split(' ', 2)
+
+    case command
+    when 'help'
+        m.user.notice('Syntax: "<lang-lang-...> <words>". If you use ' +
+                      'Esperanto (eo) for either of the languages, ' +
+                      'lernu.net single-word translation will be used. ' +
+                      'Otherwise, Bing Translator is used.')
+
+        m.user.notice('Lernu.net languages: ' + LERNU_LANGS.join(', '))
+        m.user.notice('Bing languages: ' + BING_LANGS.join(', '))
+    when 'en-sentence'
+      doc = Nokogiri::HTML(Net::HTTP.post_form(TRADUKU_URL, {'en_eo_apertium' => 'EN → EO', 't' => args}).body)
+      res = doc.at_css('#rezulto')
+      m.reply(res.inner_text.strip.gsub(/\s+/, ' '), true) if res
+    when 'eo-sentence'
+      doc = Nokogiri::HTML(Net::HTTP.post_form(TRADUKU_URL, {'eo_en_apertium' => 'EO → EN', 't' => args}).body)
+      res = doc.at_css('#rezulto')
+      m.reply(res.inner_text.strip.gsub(/\s+/, ' '), true) if res
+    else
+      langs = command.split('-')
+
+      if langs.count > @max_chain_length
+        m.reply("You may use only #{@max_chain_length} languages in a chain.", true)
+        next
+      end
+
+      # If at least one of the languages is Esperanto (eo), use lernu.net.
+      # Otherwise, use Bing Translator.
+      if langs.count == 2 && LERNU_LANGS.include?(langs[0]) && LERNU_LANGS.include?(langs[1]) && (langs[0] == 'eo' || langs[1] == 'eo')
+        words = lernu_translate(langs[0], langs[1], args)
+
+        case words.length
+        when 0
+          m.reply("No results!")
+        when 1
+          m.reply(words[0].join)
+        else
+          if w = words.find{|w| w[0] == args }
+            m.reply(w.join)
+            words.each{|w| puts w.join }
+          else
+            m.reply(words.map(&:first).join(', '))
+          end
+        end
+      elsif langs.count > 1 && langs.all?{|lang| BING_LANGS.include?(lang) }
+        text = args
+        langs.each_cons(2) do |fromlang, tolang|
+          text = @bing.translate(text, :from => fromlang, :to => tolang)
+        end
+        m.reply(text, true)
+      elsif langs.count == 2 && %w[det detect].include?(langs[0]) && BING_LANGS.include?(langs[1])
+        detected_fromlang = @bing.detect(args)
+        m.reply("(Detected as #{detected_fromlang}) " + @bing.translate(args, :from => detected_fromlang, :to => langs[1]))
+      else
+        m.reply('Invalid syntax. Try "help".', true)
+      end
+    end
+  end
+end
 
 # This method is an abomination. It was translated from a Python plasmoid
 # interface to lernu.net.
@@ -67,76 +145,6 @@ def lernu_translate(fromlang, tolang, args)
   end
 
   words
-end
-
-bot = Cinch::Bot.new do
-  configure do |c|
-    c.server   = "irc.tenthbit.net"
-    c.nick     = "traduku"
-    c.realname = "Universal translator. Uses lernu.net for Esperanto."
-    c.channels = ["#bots", "#programming", "#offtopic"]
-  end
-
-  on :message, /^#{Regexp.escape(nick)}\S*[:,]\s*(.+)$/ do |m, word|
-    command, args = word.split(' ', 2)
-
-    case command
-    when 'help'
-        m.user.notice('Syntax: "<lang-lang-...> <words>". If you use ' +
-                      'Esperanto (eo) for either of the languages, ' +
-                      'lernu.net single-word translation will be used. ' +
-                      'Otherwise, Bing Translator is used.')
-
-        m.user.notice('Lernu.net languages: ' + LERNU_LANGS.join(', '))
-        m.user.notice('Bing languages: ' + BING_LANGS.join(', '))
-    when 'en-sentence'
-      doc = Nokogiri::HTML(Net::HTTP.post_form(TRADUKU_URL, {'en_eo_apertium' => 'EN → EO', 't' => args}).body)
-      res = doc.at_css('#rezulto')
-      m.reply(res.inner_text.strip.gsub(/\s+/, ' '), true) if res
-    when 'eo-sentence'
-      doc = Nokogiri::HTML(Net::HTTP.post_form(TRADUKU_URL, {'eo_en_apertium' => 'EO → EN', 't' => args}).body)
-      res = doc.at_css('#rezulto')
-      m.reply(res.inner_text.strip.gsub(/\s+/, ' '), true) if res
-    else
-      langs = command.split('-')
-
-      if langs.count > MAX_CHAIN_LENGTH
-        m.reply("You may use only #{MAX_CHAIN_LENGTH} languages in a chain.", true)
-        next
-      end
-
-      # If at least one of the languages is Esperanto (eo), use lernu.net.
-      # Otherwise, use Bing Translator.
-      if langs.count == 2 && LERNU_LANGS.include?(langs[0]) && LERNU_LANGS.include?(langs[1]) && (langs[0] == 'eo' || langs[1] == 'eo')
-        words = lernu_translate(langs[0], langs[1], args)
-
-        case words.length
-        when 0
-          m.reply("No results!")
-        when 1
-          m.reply(words[0].join)
-        else
-          if w = words.find{|w| w[0] == args }
-            m.reply(w.join)
-            words.each{|w| puts w.join }
-          else
-            m.reply(words.map(&:first).join(', '))
-          end
-        end
-      elsif langs.count > 1 && langs.all?{|lang| BING_LANGS.include?(lang) }
-        text = args
-        langs.each_cons(2) do |fromlang, tolang|
-          text = bt.translate(text, :from => fromlang, :to => tolang)
-        end
-        m.reply(text, true)
-      elsif langs.count == 2 && %w[det detect].include?(langs[0]) && BING_LANGS.include?(langs[1])
-        detected_fromlang = bt.detect(args)
-        m.reply("(Detected as #{detected_fromlang}) " + bt.translate(args, :from => detected_fromlang, :to => langs[1]))
-      else
-        m.reply('Invalid syntax. Try "help".', true)
-      end
-    end
-  end
 end
 
 bot.start
